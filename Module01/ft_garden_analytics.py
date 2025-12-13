ea39ba74b9edd0dbf71ca74f391a978b6f621b25
thribeir <thiagoribeirochico@gmail.com>
2025-12-13 20:23:37 +0000

    encapsulation in the ex06 of Module01

--- Module01/ft_garden_analytics.py
+++ Module01/ft_garden_analytics.py
@@ -1,13 +1,69 @@
+class Garden:
+    gardens = []
+
+    def __init__(self, name):
+        self.name = name
+        self.plants = []
+        Garden.gardens += [self]
+    def add_plant(self, plant):
+        self.plants += [plant]
+        print(f"Added {plant.name} to {self.name}'s garden")
+    class Analytics:
+        @staticmethod
+        def calculate_total_plants():
+            count = 0
+            for garden in Garden.gardens:
+                for plant in garden.plants:
+                    count += 1
+            print(f"Total plants: {count}")
+        @staticmethod
+        def average_age(garden):
+            total_age = 0
+            count = 0
+            for plant in garden.plants:
+                total_age += plant.age
+                count += 1
+            average = total_age / count
+            print(f"Average age of the garden: {average}")
+
+
 class Plant:
 
     def __init__(self, name, height, age):
 
         self.name = name
-        self.height = height
-        self.age = age
+        self.__height = 0
+        self.__age = 0
+        self.set_height(height)
+        self.set_age(age)
 
-    def get_info(self):
-        print(f"{self.name}: {self.height}cm, {self.age} days old")
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, new_height):
+        if new_height >= 0:
+            self.__height = new_height
+        else:
+            print(f"\nInvalid operation attempted: height {new_height}cm [REJECTED]")
+            print("Security: Negative height rejected")
+            print(f"\nCurrent plant: {self.name} ({self.get_height()}cm, {self.get_age()} days)")
+
+    def get_age(self):
+        return self.__age
+
+    def set_age(self, new_age):
+        if new_age >= 0:
+            self.__age = new_age
+        else:
+            print(f"\nInvalid operation attempted: age {new_age} days [REJECTED]")
+            print("Security: Negative age rejected")
+            print(f"\nCurrent plant: {self.name} ({self.get_height()}cm, {self.get_age()} days)")
+
+    def grow(self):
+        self.__height += 1
+
+    def increase_age(self):
+        self.__age += 1
 
 class Flower(Plant):
     def __init__(self, name, height, age, color):
@@ -16,7 +72,7 @@ class Flower(Plant):
     def bloom(self):
             print(f"{self.name} is blooming beautifully!")
     def get_info(self):
-        print(f"{self.name} (Flower): {self.height}cm, {self.age} days, {self.color} color")
+        print(f"{self.name} (Flower): {self.get_height()}cm, {self.get_age()} days, {self.color} color")
         self.bloom()
 
 class Tree(Plant):
@@ -27,7 +83,7 @@ class Tree(Plant):
         shade = (self.trunk_diameter * self.trunk_diameter * 3.14) / 100
         print(f"{self.name} Provides {shade} square meters of shade")
     def get_info(self):
-        print(f"{self.name} (Tree): {self.height}cm, {self.age} days, {self.trunk_diameter}cm diameter")
+        print(f"{self.name} (Tree): {self.get_height()}cm, {self.get_age()} days, {self.trunk_diameter}cm diameter")
         self.produce_shade()
 
 class Vegetable(Plant):
@@ -36,6 +92,6 @@ class Vegetable(Plant):
         self.harvest_season = harvest_season
         self.nutritional_value = nutritional_value
     def get_info(self):
-        print(f"{self.name} (Vegetable): {self.height}cm, {self.age} days, {self.harvest_season} harvest")
+        print(f"{self.name} (Vegetable): {self.get_height()}cm, {self.get_age()} days, {self.harvest_season} harvest")
         print(f"{self.name} is rich in {self.nutritional_value}")
 
